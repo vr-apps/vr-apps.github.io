a674db5813f9d866291770b9cde8c350b1dd2820
Evgenii Novikov <enovikov11@yandex.ru>
2021-08-14 20:05:43 +0300

    fix #3

--- src/GameCard.js
+++ src/GameCard.js
@@ -5,6 +5,12 @@ export function GameCard({ app: { name, developer, js, thumbnail }, onPlay }) {
     return (
         <Card sx={{ maxWidth: 345, margin: "12px" }}>
             <CardActionArea onClick={() => { onPlay(js) }}>
+                <span style={{
+                    fontSize: "70px",
+                    position: "absolute",
+                    top: "calc(100px - 35px)",
+                    left: "calc(150px - 35px)"
+                }}>▶️</span>
                 <CardMedia
                     sx={{ height: 200, width: 300 }}
                     image={thumbnail}
